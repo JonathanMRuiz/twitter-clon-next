a5373b49858569eb05ccae1ffd3b9177605b04da
JonathanMRuiz <jonimrz@yahoo.com>
2022-09-23 16:51:54 -0300

    google providers

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,15 @@ import Head from "next/head";
 import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
 import styles from "../styles/Home.module.css";
+import { getProviders, getSession, useSession } from "next-auth/react";
+import Login from "../components/Login";
 
-export default function Home() {
+export default function Home({ trendingResults, followResults, providers }) {
+  const { data: session } = useSession();
+
+  if (!session) {
+    return <Login providers={providers} />;
+  }
   return (
     <div className={styles.container}>
       <Head>
@@ -19,4 +26,27 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const trendingResults = await fetch("https://www.jsonkeeper.com/b/NKEV").then(
+    (res) => res.json()
+  );
+
+  const followResults = await fetch("https://www.jsonkeeper.com/b/WWMJ").then(
+    (res) => res.json()
+  );
+
+  const providers = await getProviders();
+
+  //const session = await getSession(context);
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      //session,
+    },
+  };
 }
