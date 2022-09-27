91f044baaf444d4be4342b075ad0046a0511c4a8
JonathanMRuiz <jonimrz@yahoo.com>
2022-09-27 07:35:56 -0300

    last changes

--- components/Feed.js
+++ components/Feed.js
@@ -1,9 +1,25 @@
 import { SparklesIcon } from "@heroicons/react/outline";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Input from "./Input";
 import { useSession } from "next-auth/react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
+import Post from "./Post";
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    [db]
+  );
+
   const styles = {
     feed: "text-white flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]",
   };
@@ -17,6 +33,11 @@ const Feed = () => {
         </div>
       </div>
       <Input />
+      <div className="pb-72">
+        {posts.map((post) => (
+          <Post key={post.id} id={post.id} post={post.data()} />
+        ))}
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,13 @@ import Sidebar from "../components/Sidebar";
 import styles from "../styles/Home.module.css";
 import { getProviders, getSession, useSession } from "next-auth/react";
 import Login from "../components/Login";
+import Modal from "../components/Modal";
+import { modalState } from "../atoms/modalAtom";
+import { useRecoilState } from "recoil";
 
 export default function Home({ trendingResults, followResults, providers }) {
   const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useRecoilState(modalState);
 
   if (!session) {
     return <Login providers={providers} />;
@@ -23,7 +27,7 @@ export default function Home({ trendingResults, followResults, providers }) {
         <Feed />
         {session.user.name}
         {/* Widgets */}
-        {/* Modal */}
+        {isOpen && <Modal />}
       </main>
     </div>
   );
